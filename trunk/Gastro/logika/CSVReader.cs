4e14cf0ec2910505266bdc003eb8580bddb8aa95
gucio1414@gmail.com <gucio1414@gmail.com@1be163d8-d592-a132-510e-22ab21562873>
2011-03-20 00:31:06 +0000

    Importing data from file to column: Produkty

--- trunk/Gastro/logika/CSVReader.cs
+++ trunk/Gastro/logika/CSVReader.cs
@@ -9,6 +9,25 @@ using System.Diagnostics;
 
 namespace Gastro.logika
 {
+    class Fields
+    {
+        static public int[] aminokwasy = { 64, 81 };
+        static public int[] energia = { 86, 89 };
+        static public int[] kw_tluszcz_jednonienasyc = { 46, 53 };
+        static public int[] kw_tluszcz_nasycone = { 41, 52 };
+        static public int[] kw_tluszcz_wielonienasyc = { 54, 62 };
+        static public int[] weglowowany = { 82, 85 };
+        static public int[] witaminy = { 22, 33 };
+        static public int[] skladniki_mineralne = { 13, 21 };
+        static public int[] wartosc_energetyczna = { 4, 5 };
+        static public int[] bialko = { 7, 8 };
+        static public int choresterol = 63;
+        static public int odpadki = 3;
+        static public int woda = 6;
+        static public int tluszcz = 10;
+        static public int popiol = 12;
+    }
+
     class CSVReader
     {
         /// <summary>
@@ -36,10 +55,60 @@ namespace Gastro.logika
             {
                 // Here reading rows with data from file
                 splitedLine = line.Split(';');
-                Debug.WriteLine(splitedLine.ToString());
+                prepareProdukty(splitedLine);
+                //prepareAminokwasy(splitedLine);
+                //Debug.WriteLine(splitedLine[86].ToString());
             }
 
             stream.Close();
+            return true;
+        }
+
+        private static bool prepareProdukty(string[] entryLine)
+        {
+            Produkty prod = new Produkty();
+
+            prod.nazwa_produktu = entryLine[2];
+            prod.numer_kodowy = entryLine[1];
+            prod.odpadki = Fields.odpadki;
+            prod.popiol = Fields.popiol;
+            prod.tluszcz = Fields.tluszcz;
+            prod.woda = Fields.woda;
+            prod.choresterol = Fields.choresterol;
+            prod.bialko_zwierzece = Fields.bialko[0];
+            prod.bialko_roslinne = Fields.bialko[1];
+
+            DBProvider.addProdukty(prod);
+            return true;
+        }
+        private static bool prepareAminokwasy(string[] entryLine)
+        {
+            int counter = Fields.aminokwasy[0];
+
+            Aminokwasy amin = new Aminokwasy();
+            amin.numer_kodowy = entryLine[1].ToString();
+
+            amin.izoleucyna = Int32.Parse(entryLine[counter++]);
+            amin.leucyna = Int32.Parse(entryLine[counter++]);
+            amin.lizyna = Int32.Parse(entryLine[counter++]);
+            amin.metionina = Int32.Parse(entryLine[counter++]);
+            amin.cystyna = Int32.Parse(entryLine[counter++]);
+            amin.fenyloalanina = Int32.Parse(entryLine[counter++]);
+            amin.tyrozyna = Int32.Parse(entryLine[counter++]);
+            amin.treonina = Int32.Parse(entryLine[counter++]);
+            amin.tryptofan = Int32.Parse(entryLine[counter++]);
+            amin.walina = Int32.Parse(entryLine[counter++]);
+            amin.arginina = Int32.Parse(entryLine[counter++]);
+            amin.histydyna = Int32.Parse(entryLine[counter++]);
+            amin.alanina = Int32.Parse(entryLine[counter++]);
+            amin.kwas_asparaginowy = Int32.Parse(entryLine[counter++]);
+            amin.kwas_glutaminowy = Int32.Parse(entryLine[counter++]);
+            amin.glicyna = Int32.Parse(entryLine[counter++]);
+            amin.prolina = Int32.Parse(entryLine[counter++]);
+            amin.seryna = Int32.Parse(entryLine[counter++]);
+
+            DBProvider.addAminokwasy(amin);
+
             return true;
         }
 
--- trunk/Gastro/logika/DBProvider.cs
+++ trunk/Gastro/logika/DBProvider.cs
@@ -7,5 +7,42 @@ namespace Gastro.logika
 {
     class DBProvider
     {
+        static LinqGastroDataContext cont = new LinqGastroDataContext();
+
+        static private void myFunc()
+        {
+            var sel = from wit in cont.Witaminies
+                      select wit.D;
+
+            Witaminy wita = new Witaminy();
+            wita.D = 0;
+            cont.Witaminies.InsertOnSubmit(wita);
+            cont.SubmitChanges();
+        }
+
+        static public bool addWitaminy(Witaminy newEntry)
+        {
+            cont.Witaminies.InsertOnSubmit(newEntry);
+            cont.SubmitChanges();
+
+            return true;
+        }
+        
+        static public bool addProdukty(Produkty newEntry)
+        {
+            cont.Produkties.InsertOnSubmit(newEntry);
+            cont.SubmitChanges();
+
+            return true;
+        }
+
+
+        static public bool addAminokwasy(Aminokwasy newEntry)
+        {
+            cont.Aminokwasies.InsertOnSubmit(newEntry);
+            cont.SubmitChanges();
+
+            return true;
+        }
     }
 }
