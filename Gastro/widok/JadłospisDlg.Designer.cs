6705d3434b358afc8054140d4b9fb4fc0b8b0eca
163942 <163942@student.pwr.wroc.pl>
2011-04-25 21:55:54 +0000

    Literówka.

--- Gastro/widok/JadłospisDlg.Designer.cs
+++ Gastro/widok/JadłospisDlg.Designer.cs
@@ -130,9 +130,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 15;
-            this.label1.Text = "Filtruj jadłospis: ";
+            this.label1.Text = "Filtruj potrawy: ";
             // 
             // tbNazwaProduktu
             // 
